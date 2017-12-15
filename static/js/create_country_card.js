2028432b41555b105f103e2fae8e712e14e5f0dd
Timothée Lottaz <timozattol@gmail.com>
2017-12-15 12:26:45 +0100

    Fix indent

--- static/js/create_country_card.js
+++ static/js/create_country_card.js
@@ -55,59 +55,60 @@ function update_country_card(country) {
     const story_year = get_country_data("WLD").years;
     let graph_data = [];
 
-  for (let i = 0; i < story_year.length; ++i) {
-    let trades_per_year = {};
-    trades_per_year['year'] = story_year[i];
-    const year_index = years.indexOf(story_year[i]);
-    if( year_index == -1  ) {
-      trades_per_year['trade'] = 0;
-      graph_data.push(trades_per_year)
-    } else {
-      trades_per_year['trade'] = trades[year_index];
-      graph_data.push(trades_per_year)
-    }
+    for (let i = 0; i < story_year.length; ++i) {
+        let trades_per_year = {};
+        trades_per_year['year'] = story_year[i];
+        const year_index = years.indexOf(story_year[i]);
 
-  }
-
-  years = graph_data.map( y => y.year);
-  trades = graph_data.map( y => y.trade);
-
-  if (years.length < 3) {
-    console.log("TODO manage countries with few data");
-  }
-
-
-  [background_color, border_color] = get_colors();
-
-  let point_border_colors = []
-  let point_background_colors = []
-  let point_style = [];
-  for (let t of trades) {
-    if (t == 0) {
-      //point_style.push('crossRot');
-      point_border_colors.push("#BFBFBF");
-      point_background_colors.push("#EEEEEE");
-    } else {
-      //point_style.push('circle');
-      point_border_colors.push(border_color);
-      point_background_colors.push(border_color);
+        if( year_index == -1  ) {
+            trades_per_year['trade'] = 0;
+            graph_data.push(trades_per_year)
+        } else {
+            trades_per_year['trade'] = trades[year_index];
+            graph_data.push(trades_per_year)
+        }
 
     }
-  }
 
-  country_card_chart.data = {
-    labels: years,
-    datasets: [{
-      label: 'Value of trades ($)',
-      data: trades,
-      backgroundColor: background_color,
-      borderColor: border_color,
-      borderWidth: 1,
-      pointStyle: 'circle',//point_style,
-      pointBorderColor: point_border_colors,
-      pointBackgoundColor: point_background_colors
-    }],
-  };
+    years = graph_data.map( y => y.year);
+    trades = graph_data.map( y => y.trade);
 
-  country_card_chart.update();
+    if (years.length < 3) {
+        console.log("TODO manage countries with few data");
+    }
+
+
+    [background_color, border_color] = get_colors();
+
+    let point_border_colors = []
+    let point_background_colors = []
+    let point_style = [];
+    for (let t of trades) {
+        if (t == 0) {
+            //point_style.push('crossRot');
+            point_border_colors.push("#BFBFBF");
+            point_background_colors.push("#EEEEEE");
+        } else {
+            //point_style.push('circle');
+            point_border_colors.push(border_color);
+            point_background_colors.push(border_color);
+
+        }
+    }
+
+    country_card_chart.data = {
+        labels: years,
+        datasets: [{
+            label: 'Value of trades ($)',
+            data: trades,
+            backgroundColor: background_color,
+            borderColor: border_color,
+            borderWidth: 1,
+            pointStyle: 'circle',//point_style,
+            pointBorderColor: point_border_colors,
+            pointBackgoundColor: point_background_colors
+        }],
+    };
+
+    country_card_chart.update();
 }
