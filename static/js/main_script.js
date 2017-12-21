eb8544851993fe56b672cbf7fd3a438bc2911510
julied20 <julie@djeff.al>
2017-12-21 16:21:01 +0100

    Add country on navbar and start writting aboug palm oil

--- static/js/main_script.js
+++ static/js/main_script.js
@@ -22,7 +22,7 @@ stories.forEach((story, index) => {
                 const split_id = this.id.split('_')
                 change_story(parseInt(split_id[split_id.length - 1]));
             })
-            .text(story.product_name);
+            .html('<b>' + story.product_name + '</b> <br> <i>' + story.country_name + '</i>');
 
         nav_exploration_ul
             .append('li')
@@ -36,7 +36,7 @@ stories.forEach((story, index) => {
                     const split_id = this.id.split('_')
                     change_story(parseInt(split_id[split_id.length - 1]));
                 })
-                .text(story.product_name);
+                .html('<b>' + story.product_name + '</b> <br> <i>' + story.country_name + '</i>');
         }
 });
 
@@ -73,7 +73,7 @@ stories.forEach((story, index) => {
                 change_story(parseInt(split_id[split_id.length - 1]));
                 nav_dropdown_menu.attr('display', 'none')
             })
-            .text(story.product_name)
+            .html('<b>' + story.product_name + '</b>  <i>' + story.country_name + '</i>')
         }
 });
 
